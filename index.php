029a75e6f146a2799c088d8b0322a7c1ef72ba6f
Essietom <essietom@gmail.com>
2018-04-11 20:23:29 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNGinternship</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="container">
+	
+<div class="menu-bar">
+				<div class="namelogo"><b>HNG</b> Internship<br></div>
+				<nav class="navbar">
+					<ul class="menu">
+						<li><a href="">Home</a></li>
+						<li><a href="">About</a></li>
+						<li><a href="">Connect</a></li>
+						
+					</ul>
+				</nav>
+				
+</div><!--end of  div menu-bar-->
+
+<div class="timer"><?php echo date("h:i:sa")  ?></div>
+<div class="foot "><div class="footer">&copy Copyright 2018, HNGInternship4</div></div>
+</div>
+</body>
+</html>
